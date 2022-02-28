8a156df9889cba684eb253803ab90e1ca55f9573
kodithuw <kodithuw@adobe.com>
2022-02-28 22:01:40 +1100

    update ticket

--- actions/cloud_check_disk.sh
+++ actions/cloud_check_disk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CLOUDENV=${1}
 if [ ! -z "$CLOUDENV" ];then
-  magento-cloud environment:ssh -p $CLOUDENV -e staging "df -h"
+  magento-cloud environment:ssh -p $CLOUDENV -e production "df -h"
 else
   echo "Cloud Name Not Set"
   exit 1
