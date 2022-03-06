40039e922b5fdf7b3fbca89c3fd8c4fff021f2f3
kodithuw <kodithuw@adobe.com>
2022-03-06 11:22:21 +1100

    error handling

--- actions/verify_env.sh
+++ actions/verify_env.sh
@@ -2,12 +2,13 @@
 CLOUDENVSTR=${1}
 CLOUDENV=$(echo $CLOUDENVSTR | sed  's/^.*(\(.*\)).*$/\1/g')
 if [ ! -z "$CLOUDENV" ];then
-  SSHCHECK=$(magento-cloud ssh -p $CLOUDENV -e production echo ok 2>&1 | grep "ok")
-  if [[ ! -z "$SSHCHECK" && $SSHCHECK == "ok" ]];then
+  SSHCHECK=$(magento-cloud ssh -p $CLOUDENV -e production echo canconnect 2>&1)
+  SSHCHECKOK=$(echo $SSHCHECK | grep canconnect)
+  if [[ ! -z "$SSHCHECKOK" ]];then
     echo $CLOUDENV
     exit 0
   else
-    echo "Cloud SSH Failed"
+    echo "Cloud SSH Failed --> $SSHCHECK"
     exit 1
   fi
 else
