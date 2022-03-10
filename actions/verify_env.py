d3e23a4ecb705b18a2ef1995698f882f208fc210
Aneesh Raj <anraj@adobe.com>
2022-03-10 19:47:26 +1100

    updated the entrypoint verify_env.py

---
+++ actions/verify_env.py
@@ -0,0 +1,23 @@
+import re
+import sys
+import subprocess
+
+def check_ssh_connection(CLOUDENV):
+    cmd_args=["magento-cloud","-q","ssh", "-p",CLOUDENV, "-e", "production","2>/dev/null" ]
+    ssh_check=subprocess.run(cmd_args,shell=False)
+    return(ssh_check.returncode)
+
+if __name__=="__main__":
+    if len(sys.argv) < 2:
+        raise ValueError("Usage: check_env.py <project_id>")
+
+    CLOUDENV=re.findall("\([a-z0-9]*\)",sys.argv[1])[0][1:-1]
+
+    if (not CLOUDENV):
+        raise Exception("Invalid project details")
+    else:
+        ssh_status=check_ssh_connection(CLOUDENV)
+        if (ssh_status == 0):
+            print(CLOUDENV)
+        else:
+            raise Exception("Magento Cloud SSH failed!")
