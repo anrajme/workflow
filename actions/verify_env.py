dffc1330b5b161e6fd3741492a1dc0ab13acfbb3
Aneesh Raj <anraj@adobe.com>
2022-03-10 23:59:46 +1100

    Removed the main function

--- actions/verify_env.py
+++ actions/verify_env.py
@@ -4,23 +4,17 @@ import subprocess
 from st2common.runners.base_action import Action
 
 class VerifySSHConnection(Action):
-    def run(self,CLOUDENV):
+    def run(self,accountname):
+        CLOUDENV=re.findall("\([a-z0-9]*\)",sys.argv[1])[0][1:-1]
+        if (not CLOUDENV):
+            raise Exception("Invalid project details")
         cmd_args=["/home/stanley/.magento-cloud/bin/magento-cloud","-q","ssh", "-p",CLOUDENV, "-e", "production","2>/dev/null" ]
         ssh_check=subprocess.run(cmd_args,shell=False)
         return(ssh_check.returncode)
 
-if __name__=="__main__":
-    if len(sys.argv) < 2:
-        raise ValueError("Usage: check_env.py <project_id>")
-
-    CLOUDENV=re.findall("\([a-z0-9]*\)",sys.argv[1])[0][1:-1]
-
-    if (not CLOUDENV):
-        raise Exception("Invalid project details")
-    else:
-        ssh_status=VerifySSHConnection()
-        ssh_return=ssh_status.run(CLOUDENV)
-        if (ssh_return == 0):
-            print(CLOUDENV)
-        else:
-            raise Exception("Magento Cloud SSH failed!")
+ssh_status=VerifySSHConnection()
+ssh_return=ssh_status.run(CLOUDENV)
+if (ssh_return == 0):
+    print(CLOUDENV)
+else:
+    raise Exception("Magento Cloud SSH failed!")
