c5020984bd4a385eab9790961324e287893315f7
Aneesh Raj <anraj@adobe.com>
2022-03-14 23:44:46 +1100

    fixed issues linked to os env

--- actions/verify_env.py
+++ actions/verify_env.py
@@ -1,16 +1,22 @@
 import re
 import sys
 import subprocess
+import os
+
 from st2common.runners.base_action import Action
 
 class VerifySSHConnection(Action):
-    def run(self,accountname):
-        CLOUDENV=re.findall("\([a-z0-9]*\)",accountname)[0][1:-1]
-        if (not CLOUDENV):
+    def run(self,accountname,cloudtoken):
+        cloudenv=re.findall("\([a-z0-9]*\)",accountname)[0][1:-1]
+        if (not cloudenv):
             raise Exception("Invalid project details")
-        cmd_args=["/home/stanley/.magento-cloud/bin/magento-cloud","-q","ssh", "-p",CLOUDENV, "-e", "production","2>/dev/null" ]
-        ssh_check=subprocess.run(cmd_args,shell=False)
-        if (ssh_check.returncode == 0):
-            print(CLOUDENV)
         else:
-            raise Exception("Magento Cloud SSH failed!")
+            os.environ['MAGENTO_CLOUD_CLI_TOKEN'] = cloudtoken
+
+            # TODO : needs to use subprocess instead of os.system
+
+            exec_status=os.system('/home/stanley/.magento-cloud/bin/magento-cloud auth:api-token-login -y -q; /usr/bin/php /home/stanley/.magento-cloud/bin/magento-cloud ssh -p %s -e production 2>&1 " "' % (cloudenv))
+            if (exec_status == 0):
+                print("Successfully connected to Magento Cloud Instance via SSH")
+            else:
+                raise Exception("SSH conection attempt failed!")
